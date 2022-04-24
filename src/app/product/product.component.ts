5b411ca5eccd2bf9040384ebd5dc8821f84d40f5
Iezham Arief <102403018+iezhamarief@users.noreply.github.com>
2022-04-24 16:41:26 +0800

    -data binding

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -2,10 +2,19 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'product',
-  template: '<p>2product works!</p>',
+  templateUrl: './product.component.html',
   styleUrls: ['./product.component.css']
 })
 export class ProductComponent implements OnInit {
+
+  /// string interpolition
+  productID:number=1;
+  stockStats:string='Yes';
+
+  getStockStats(){
+
+    return this.stockStats;
+  }
 
   constructor() { }
 
--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -8,4 +8,28 @@ import { Component } from "@angular/core";
 
 export class ProductsComponent{
 
+  ///button- property binding
+  addProduct=true;
+  addProductStatus='No Product Added!';
+  enteredVal='Product 1';
+
+  constructor(){
+      
+    setTimeout(()=> {
+
+        this.addProduct=false
+    },4000)
+  }
+
+  onClick(){
+
+    this.addProductStatus= this.enteredVal +' Added! is';
+  }
+
+  onUpdateProductName(event:any){
+
+    console.log(event);
+    this.enteredVal=(<HTMLInputElement>event.target).value;
+
+  }
 }
